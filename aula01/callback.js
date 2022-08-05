d09df112c34821fdebc6236fe11b30ad56b02771
Brendon-Nogueira <brendonnogueira21@gmail.com>
2022-08-05 20:51:45 -0300

    update

--- aula01/callback.js
+++ aula01/callback.js
@@ -1,1 +1,11 @@
-console.log('Hello Word!!')
+console.log('hello world!!!')
+
+const sum = (a, b, callback) => {
+    callback(a + b)
+}
+
+const myFunctionCallback = value => {
+    console.log(`Valor: ${value}`)
+}
+
+sum(10, 5, myFunctionCallback)
